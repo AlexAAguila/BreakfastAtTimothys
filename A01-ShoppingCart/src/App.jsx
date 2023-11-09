688d39cd299bb3e4e16dec38baacce419e20d540
Maria Revelo <fer_nandita_8@hotmail.com>
2023-11-09 12:13:55 -0800

    Finished

--- A01-ShoppingCart/src/App.jsx
+++ A01-ShoppingCart/src/App.jsx
@@ -1,35 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const [cart,setCart] = useState([]);
+ 
+const breakfast ={
+title:"Shopping Cart",
+cart:[],
+ products: ["French Vanilla", "Black Coffee", "Pumpkin Spice Latte", "Sausy Farmer Wrap", "Wild Blueberry Muffin", "Chicken Noodle Soup", "Honey Cruller", "Boston Cream", "Apple Fritter", "Honey Dip", "Everything Bagel", "Bacon Breakfast Sandwich", "Green Tea", "Double Double"]
+}
+
+const addToCart = (e)=>{
+  e.preventDefault();
+const productName = e.target.elements.products.value;
+const existingProduct = cart.find(item => item.name === productName)
+
+if (existingProduct) {
+  setCart(cart.map(item =>
+    item.name === productName? {...item, quantity:item.quantity +1} :item
+   
+  ))
+}else {
+  setCart([...cart, {name: productName,quantity :1}]);
+}
+
+};
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+ <h1>{breakfast.title}</h1>
+<form onSubmit={addToCart}>
+<label htmlFor="products">Products:</label>
+
+<select name="products" id="products">
+  {breakfast.products.map((product,index) =>
+  <option key={index} value={product}>{product}</option>
+)}</select>
+<button type='submit'>Add to Cart</button>
+</form>
+
+{cart.length > 0 && (
+  <div className='cart-items'>
+    <ul>
+      {cart.map((item,index) =>(
+        <li key={index}>{item.name}{item.quantity > 1 && ` x ${item.quantity}`}</li>
+
+      ))}
+    </ul>
+  </div>
+)}
+<p>{cart.length === 0? 'Add Some Items' : `You have ${cart.reduce((total,item)=> total + item.quantity,0)} item(s) in your cart`}</p>
+
     </>
-  )
+)
 }
 
-export default App
+
+export default App;
